fae302cc04aea837da811827b430e950a9a39812
BarthesSimpson <d.s.frenk@gmail.com>
2018-10-21 09:37:11 -0700

    enable multi-filtering

--- TODO.md
+++ TODO.md
@@ -1,12 +1,12 @@
 # StableTable:TODO
 
-1. Add sorting
-2. Add filtering
+1. Add multi-filtering
 3. Add tree data rendering
 
 ## Later
 
 1. Add theming
 2. Nested field accessor
-3. Function field accesor
+3. Function field accessor
 4. Pagination
+5. Additional filter types
--- src/table/data/DataStore.js
+++ src/table/data/DataStore.js
@@ -5,23 +5,41 @@ import { filterRowsOnField } from '@data/filtering'
 class DataStore extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { rows: props.rowData }
+    this.state = { rows: props.rowData, filters: {} }
+    this.applyFilters = this.applyFilters.bind(this)
     this.sortByField = this.sortByField.bind(this)
     this.filterOnField = this.filterOnField.bind(this)
   }
 
+  applyFilters() {
+    const { rows, filters } = this.state
+    return Object.values(filters).reduce(
+      (filteredRows, filter) => filter(filteredRows),
+      rows
+    )
+  }
+
   sortByField({ field, descending }) {
     const { rowData: rows } = this.props
-    this.setState({ rows: sortRowsByField({ rows, field, descending }) })
+    this.setState({
+      ...this.state,
+      rows: sortRowsByField({ rows, field, descending })
+    })
   }
 
   filterOnField({ field, value }) {
-    const { rowData: rows } = this.props
-    this.setState({ rows: filterRowsOnField({ rows, field, value }) })
+    const { filters } = this.state
+    this.setState({
+      ...this.state,
+      filters: {
+        ...filters,
+        [field]: rows => filterRowsOnField({ rows, field, value })
+      }
+    })
   }
 
   render() {
-    const { rows } = this.state
+    const rows = this.applyFilters()
     const sortByField = this.sortByField
     const filterOnField = this.filterOnField
     return this.props.children({ rows, sortByField, filterOnField })
